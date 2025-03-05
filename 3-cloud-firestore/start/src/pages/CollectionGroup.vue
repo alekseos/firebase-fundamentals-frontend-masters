d1253464cd2be8c339de674eb64e8ad9b08a6294
Alekseev Andrey <alekseevandreygen@gmail.com>
2025-03-06 00:57:29 +0300

    Add lockfiles and refactor Firestore queries for improved readability !

--- 3-cloud-firestore/start/src/pages/CollectionGroup.vue
+++ 3-cloud-firestore/start/src/pages/CollectionGroup.vue
@@ -1,25 +1,25 @@
 <script setup>
-import { onMounted, reactive, onBeforeUnmount } from 'vue';
-import ExpenseExcersize from '../components/ExpenseExcersize.vue'
-import { getFirebase } from '../firebase';
-import { collection, onSnapshot, limit, query, where, orderBy, collectionGroup } from 'firebase/firestore';
+import { collection, collectionGroup, onSnapshot } from "firebase/firestore";
+import { onBeforeUnmount, onMounted, reactive } from "vue";
+import ExpenseExcersize from "../components/ExpenseExcersize.vue";
+import { getFirebase } from "../firebase";
 
 const { firestore } = getFirebase();
-const expensesCol = collection(firestore, 'expenses');
+const expensesCol = collection(firestore, "expenses");
+const expensesColGroup = collectionGroup(firestore, "expenses");
 let expensesQuery = null;
 
 // // 1. Get the first 100 expenses across all users that are under $100
-// expensesQuery = query(
-
-// );
+expensesQuery = expensesColGroup;
 
 // // 2. Get the first 100 expenses across all users from any date range
 // expensesQuery = query(
-
+//   expensesColGroup,
+//   limit(100),
+//   where("date", "==", new Date("12/08/2021"))
 // );
 
-
-const state = bindToTable(expensesQuery)
+const state = bindToTable(expensesQuery);
 
 function bindToTable(expensesQuery) {
   const state = reactive({ results: [] });
@@ -34,9 +34,9 @@ function bindToTable(expensesQuery) {
 }
 
 function bindToState(state, query, transform) {
-  return onSnapshot(query, snapshot => {
+  return onSnapshot(query, (snapshot) => {
     const callbackFn = transform == null ? formatExpense : transform;
-    state.results = snapshot.docs.map(d => {
+    state.results = snapshot.docs.map((d) => {
       return callbackFn(snapshot, d);
     });
   });
@@ -44,24 +44,23 @@ function bindToState(state, query, transform) {
 
 function formatExpense(snapshot, d) {
   const { uid, cost, categories, date: rawDate } = d.data();
-  const dateConfig = { day: '2-digit', month: '2-digit', year: 'numeric' };
-  const date = new Intl.DateTimeFormat('en', dateConfig).format(rawDate.toDate());
+  const dateConfig = { day: "2-digit", month: "2-digit", year: "numeric" };
+  const date = new Intl.DateTimeFormat("en", dateConfig).format(
+    rawDate.toDate()
+  );
   const { fromCache } = snapshot.metadata;
-  return { uid, cost, categories: categories.join(', ') , fromCache, date };
+  return { uid, cost, categories: categories.join(", "), fromCache, date };
 }
 </script>
 
-
 <template>
-  <main>    
-    <ExpenseExcersize 
+  <main>
+    <ExpenseExcersize
       number="2"
       subtitle="The basics of querying"
-      :results="state.results" />
+      :results="state.results"
+    />
   </main>
-
 </template>
 
-<style>
-
-</style>
+<style></style>
